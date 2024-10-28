8cc0fd977e8938337a1a6e2123be9b9b82a7d2b8
Arbora-Behluli <arbora.behluli2@gmail.com>
2024-10-28 14:06:42 +0100

    Updated icons.blade.php

--- resources/views/icons.blade.php
+++ resources/views/icons.blade.php
@@ -6,8 +6,8 @@
     <title>Icons</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gray-100 flex justify-center items-center h-screen">
-    <div class="flex justify-center items-center space-x-40">
+<body class="bg-gray-100 flex justify-center items-center min-h-screen p-4">
+    <div class="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-28">
         <div class="text-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="url(#verifiedGradient)" class="w-16 h-16 mx-auto">
                 <defs>
@@ -18,12 +18,12 @@
                 </defs>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
-            <h3 class="text-xl font-semibold mt-4">Verifikimi i anëtarëve</h3>
-            <p class="text-gray-500 text-sm mt-2">Verifiko profilin për ta rritur besueshmërine në<br> komunitet</p> 
+            <h3 class="text-lg md:text-xl font-semibold mt-4">Verifikimi i anëtarëve</h3>
+            <p class="text-gray-500 text-sm text-lg mt-2">Verifiko profilin për ta rritur<br>besueshmërinë në komunitet</p> 
         </div>
 
         <div class="text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="57" height="57" viewBox="0 0 24 24" fill="none" stroke="url(#starGradient)" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="w-16 h-16 mx-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" width="57" height="57" viewBox="0 0 24 24" fill="none" stroke="url(#starGradient)" stroke-width="1" stroke-linecap="steepest" stroke-linejoin="round" class="w-16 h-16 mx-auto">
                 <defs>
                     <linearGradient id="starGradient" x1="0%" y1="0%" x2="100%" y2="100%">
                         <stop offset="0%" style="stop-color:#0000FF; stop-opacity:1" />
@@ -32,8 +32,8 @@
                 </defs>
                 <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
             </svg>
-            <h3 class="text-xl font-semibold mt-4">Vlerëso udhëtimet</h3>
-            <p class="text-gray-500 text-sm mt-2">Vleresoni bashkudhëtarët pas cdo udhëtimi</p> 
+            <h3 class="text-lg md:text-xl font-semibold mt-4">Vlerëso udhëtimet</h3>
+            <p class="text-gray-500 text-sm  text-lg mt-2">Vlerësoni bashkudhëtarët <br> pas cdo udhëtimi <br></p> 
         </div>
 
         <div class="text-center">
@@ -46,8 +46,8 @@
                 </defs>
                 <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
             </svg>
-            <h3 class="text-xl font-semibold mt-4">Raporto</h3>
-            <p class="text-gray-500 text-sm mt-2">Ju mund t'i raportoni personat e<br> papërgjegjshëm</p>
+            <h3 class="text-lg md:text-xl font-semibold mt-4">Raporto</h3>
+            <p class="text-gray-500 text-sm  text-lg mt-2">Ju mund t'i raportoni<br>personat e papërgjegjshëm</p>
         </div>
     </div>
 </body>
