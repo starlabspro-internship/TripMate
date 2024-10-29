1b25079530ab1f55a788e6b154ab4671a26e57f7
ukedabiqaj <u.dabiqaj@gmail.com>
2024-10-29 10:11:52 +0100

    Update navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,10 @@
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="w-full md:absolute md:w-navbar md:left-[50px]">
-                    @include('components.navbar')
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
                 </header>
             @endisset
 
