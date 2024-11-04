01e181b992cc43d9d04ff9d564b5013ccb26f04a
Tringa Gjurka <121450403+tringagjurka@users.noreply.github.com>
2024-11-04 17:04:09 +0100

    tringa/partners-view
    added partners view

---
+++ resources/views/components/partners.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <div class=" container">
+        <h1 class="font-sans font-bold heading">Our Partners</h1>
+
+        <body style="background-color: white">
+        <div class="box-container">
+            <div class="box">
+                <img src="https://cdn.prod.website-files.com/62a1db362f4c742516937d9f/6352d94bdfc2266c5b7c0010_Kosovo-Innovation-Center.png" alt="">
+            </div>
+            <div class="box">
+                <img src="https://play-lh.googleusercontent.com/X6hnIfBh-DZXhEGZl_8LaCoJVCcluchAkGcWMb7qFmcKgaHfwVNIGswzaq1fPk4gOVI" alt="">
+            </div>
+            <div class="box">
+                <img src="https://www.123ruceni.cz/wp-content/uploads/2024/04/carvago-logo-1.png" alt="">
+            </div>
+            <div class="box">
+                <img src="https://kosovajob.com/admin/companyLogos/1616601919.png" alt="">
+            </div>
+            <div class="box">
+                <img src="https://kosovajob.com/admin/companyLogos/1629360346.png" alt="">
+            </div>
+            <div class="box">
+                <img src="https://anibar.org/2021/wp-content/uploads/2022/05/Anibar-Logos-2.png" alt="">
+            </div>
+            <div class="box">
+                <img src="https://kosovajob.com/admin/companyLogos/2102821716118848.png" alt="">
+            </div>
+            <div class="box">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxWHVzwch_YQLOlQNNaVHgkqa7sJ_qni_gVcVwa0tEDKNP3lmhl_4evheEPFJ5_DcbB_o&usqp=CAU" alt="">
+            </div>
+        </div>
+    </div>
+</x-app-layout>
