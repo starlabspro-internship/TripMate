e1569b66a1426732cbf86dc84e3bbd7d5d01ba28
rigakorcaa <rigakorqa5@gmail.com>
2024-11-04 16:20:11 +0100

    riga/edit-trip/small-changes
    fixed edit-trip view and some cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,4 @@
 <?php
-
-
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SocialAuthController;
 use Laravel\Socialite\Facades\Socialite;
@@ -10,29 +8,19 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\TripController;
 
-// Route::get('/trips', [TripController::class, 'index']);
-Route::resource('trips', TripController::class);
-Route::get('/trips', [TripController::class, 'index'])->name('trips.index');
-Route::get('/trips/create', [TripController::class, 'create'])->name('trips.create');
-Route::post('/trips/store', [TripController::class, 'store'])->name('trips.store');
-Route::post('/trips', [TripController::class, 'store']); 
-Route::get('/trips/{id}/edit', [TripController::class, 'edit']);
-Route::put('/trips/{id}', [TripController::class, 'update']); 
-Route::delete('/trips/{id}', [TripController::class, 'destroy']); 
+Route::prefix('trips')->name('trips.')->group(function () {
+    Route::get('/', [TripController::class, 'index'])->name('index');
+    Route::get('/create', [TripController::class, 'create'])->name('create');
+    Route::post('/store', [TripController::class, 'store'])->name('store');
+    Route::get('/{id}/edit', [TripController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [TripController::class, 'update'])->name('update');
+    Route::delete('/{id}', [TripController::class, 'destroy'])->name('destroy');
+});
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/banners', function () {
-    return view('banners.index');
-})
-->name('banners');
-
-//example route
-Route::get( '/example', function () {
-    return view('example.index');
-})->name(name: "example");
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -45,26 +33,8 @@ Route::middleware('auth')->prefix('profile')->group(function () {
 
 });
 
-
 Route::get('auth/google',[SocialAuthController::class, 'redirectToGoogle'])->name('login.google');
 Route::get('auth/google/callback',[SocialAuthController::class, 'googleCallback'])->name('login.google.callback');
-
-Route::get('/icons', function () {
-    return view('icons');
-    });
-
-Route::get('/cars', function () {
-    return view('cars');
-});
-
-
-    Route::get('test-superadmin', function () {
-        return 'You are a super admin!';
-    })->middleware(['auth', 'super_admin']);
-    
-  
-    
-
 
 require __DIR__.'/auth.php';
 
