032f0485ce9223c8ca837f1488b7c65fd1a89f57
ukedabiqaj <u.dabiqaj@gmail.com>
2024-11-07 16:08:23 +0100

    update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 
-<nav x-data="{ open: false }" class="fixed top-0 left-0 right-0 md:left-10 md:right-10 text-white flex justify-between bg-gray-400 items-center px-5 py-3  bg-opacity-50 backdrop-blur-xl shadow-lg rounded-lg mx-auto z-50">
+<nav x-data="{ open: false }" class="fixed top-0 left-0 right-0 md:left-10 md:right-10 text-white flex justify-between bg-gray-400 items-center px-5 py-3  bg-opacity-50 backdrop-blur-lg shadow-lg rounded-lg mx-auto z-50">
     <a class="mx-2 font-mono text-2xl font-bold " href="/">TripMate</a>
     <div :class="{'h-fit': open, 'h-0': ! open}" class="absolute w-full bg-gray-400 bg-opacity-50 md:bg-opacity-0  rounded-lg mt-4 md:mt-1 top-14 overflow-hidden  right-px px-5 md:relative md:h-fit md:top-0">
         <ul class=" flex text-lg flex-col gap-4 py-4 md:py-0 md:flex-row md:justify-center md:gap-0 ">
