b296f8f5bb0daf8d8b7c9a978b6e1b062497561e
Yll <ylli8822@gmail.com>
2024-12-04 22:33:08 +0000

    Changes to database seeder and changed database backup format

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,21 +1,29 @@
-public function run(): void
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
 {
-    User::factory()->firstOrCreate(
-        ['email' => 'admin@gmail.com'],
-        [
-            'name' => 'admin',
-            'password' => Hash::make('adminpassword'),
-        ]
-    );
-
-    User::factory()->firstOrCreate(
-        ['email' => 'superadmin@gmail.com'],
-        [
-            'name' => 'superadmin',
-            'password' => Hash::make('superadmin'),
-            'is_super_admin' => true,
-        ]
-    );
-
-    $this->call(CitySeeder::class);
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'admin',
+                'password' => Hash::make('adminpassword'),
+            ]
+        );
+        User::firstOrCreate(
+            ['email' => 'superadmin@gmail.com'],
+            [
+                'name' => 'superadmin',
+                'password' => Hash::make('superadmin'),
+                'is_super_admin' => true,
+            ]
+        );
+        $this->call(CitySeeder::class);
+    }
 }
