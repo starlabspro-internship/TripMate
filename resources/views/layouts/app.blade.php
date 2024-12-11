a50f7b342c4aa4c0f190029cc50041c6b96c19a7
Arbora-Behluli <arbora.behluli2@gmail.com>
2024-12-11 22:17:54 +0100

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,13 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>TripMate</title>
-
-     
+        
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="TripMate" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         <!-- Scripts -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js"></script>
