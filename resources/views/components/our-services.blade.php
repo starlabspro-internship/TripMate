bf033cbd134a33f3d5aa1c26fd6eb5ebb8a6ce93
Egzon-Salihi <egzonsalihi20@gmail.com>
2024-12-23 14:38:00 +0200

    Our-service

--- resources/views/components/our-services.blade.php
+++ resources/views/components/our-services.blade.php
@@ -35,7 +35,7 @@
                             Passengers
                         </button>
                     </a>
-                    <a href="#">
+                    <a href="{{route('trips.create')}}">
                         <button class="bg-blue-500 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-xl">
                             Drivers
                         </button>
