a3094276e305e24f28ccdb241438eeb45d692153
rigakorcaa <rigakorqa5@gmail.com>
2024-12-25 18:52:09 +0100

    riga/SOS-alert

--- resources/views/components/end-modal.blade.php
+++ resources/views/components/end-modal.blade.php
@@ -1,21 +1,21 @@
-<div id="popup-modal" class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+<div id="end-modal" class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
     <div class="bg-white p-6 rounded shadow-lg w-96">
-        <h2 class="text-lg font-semibold mb-4" id="popup-message">The trip has ended. Please rate your driver.</h2>
+        <h2 class="text-lg font-semibold mb-4" id="end-message">The trip has ended. Please rate your driver.</h2>
         <form action="{{ route('passenger-ratings.store') }}" method="POST">
             @csrf
-            <input type="hidden" name="trip_id" id="popup-trip-id">
-            <input type="hidden" name="reviewed_id" id="popup-driver-id">
+            <input type="hidden" name="trip_id" id="end-trip-id">
+            <input type="hidden" name="reviewed_id" id="end-driver-id">
             <input type="hidden" name="reviewer_id" value="{{auth()->id()}}">
             <input type="hidden" name="rating" id="rating-value" value="0"> <!-- Hidden input for rating -->
 
             <!-- Rating System -->
             <div id="rating" class="flex justify-center space-x-2 my-8">
                 @for ($i = 1; $i <= 5; $i++)
-                <svg 
-                    data-value="{{ $i }}" 
-                    class="w-12 h-12 text-gray-300 cursor-pointer star"  
-                    fill="currentColor" 
-                    xmlns="http://www.w3.org/2000/svg" 
+                <svg
+                    data-value="{{ $i }}"
+                    class="w-12 h-12 text-gray-300 cursor-pointer star"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24">
                     <path d="M11.2691 4.41115C11.5006 3.89177 11.6164 3.63208 11.7776 3.55211C11.9176 3.48263 12.082 3.48263 12.222 3.55211C12.3832 3.63208 12.499 3.89177 12.7305 4.41115L14.5745 8.54808C14.643 8.70162 14.6772 8.77839 14.7302 8.83718C14.777 8.8892 14.8343 8.93081 14.8982 8.95929C14.9705 8.99149 15.0541 9.00031 15.2213 9.01795L19.7256 9.49336C20.2911 9.55304 20.5738 9.58288 20.6997 9.71147C20.809 9.82316 20.8598 9.97956 20.837 10.1342C20.8108 10.3122 20.5996 10.5025 20.1772 10.8832L16.8125 13.9154C16.6877 14.0279 16.6252 14.0842 16.5857 14.1527C16.5507 14.2134 16.5288 14.2807 16.5215 14.3503C16.5132 14.429 16.5306 14.5112 16.5655 14.6757L17.5053 19.1064C17.6233 19.6627 17.6823 19.9408 17.5989 20.1002C17.5264 20.2388 17.3934 20.3354 17.2393 20.3615C17.0619 20.3915 16.8156 20.2495 16.323 19.9654L12.3995 17.7024C12.2539 17.6184 12.1811 17.5765 12.1037 17.56C12.0352 17.5455 11.9644 17.5455 11.8959 17.56C11.8185 17.5765 11.7457 17.6184 11.6001 17.7024L7.67662 19.9654C7.18404 20.2495 6.93775 20.3915 6.76034 20.3615C6.60623 20.3354 6.47319 20.2388 6.40075 20.1002C6.31736 19.9408 6.37635 19.6627 6.49434 19.1064L7.4341 14.6757C7.46898 14.5112 7.48642 14.429 7.47814 14.3503C7.47081 14.2807 7.44894 14.2134 7.41394 14.1527C7.37439 14.0842 7.31195 14.0279 7.18708 13.9154L3.82246 10.8832C3.40005 10.5025 3.18884 10.3122 3.16258 10.1342C3.13978 9.97956 3.19059 9.82316 3.29993 9.71147C3.42581 9.58288 3.70856 9.55304 4.27406 9.49336L8.77835 9.01795C8.94553 9.00031 9.02911 8.99149 9.10139 8.95929C9.16534 8.93081 9.2226 8.8892 9.26946 8.83718C9.32241 8.77839 9.35663 8.70162 9.42508 8.54808L11.2691 4.41115Z"></path>
                 </svg>
@@ -23,15 +23,15 @@
             </div>
 
             <!-- Feedback -->
-            <textarea name="feedback" class="border rounded-lg w-full p-2" placeholder="Leave feedback (optional)"></textarea>
+            <textarea name="feedback" class="border rounded-lg w-full p-2" placeholder="{{__('messages.Leave feedback (optional)')}}"></textarea>
 
             <!-- Submit Button -->
             <div class="flex justify-end gap-2 mt-4">
-                <button type="button" id="popup-close" class="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400">
-                    Cancel
+                <button type="button" id="end-close" class="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400">
+                    {{__('messages.Cancel')}}
                 </button>
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                    Submit
+                    {{__('messages.Submit')}}
                 </button>
             </div>
         </form>
@@ -49,10 +49,10 @@
                 const driver = e.target.getAttribute('data-driver-id');
                 console.log('driver:', driver);
                 const message = 'The trip has ended. Please rate your driver.';
-                showPopup(message, tripId, driver);
+                showEnd(message, tripId, driver);
             }
         });
-    }); 
+    });
 
     document.addEventListener('DOMContentLoaded', function () {
         const stars = document.querySelectorAll('.star');
@@ -62,7 +62,7 @@
     stars.forEach(star => {
         // Highlight stars on hover
         star.addEventListener('mouseover', () => {
-            highlightStars(star.getAttribute('data-value'));   
+            highlightStars(star.getAttribute('data-value'));
         });
 
         // Reset stars after hover ends
@@ -93,21 +93,21 @@
 
 
 
-        const setupPopUp = () => {
+        const setupEnd = () => {
             if (window.Echo) {
                 window.Echo.channel(`endtrip${window.auth_user.id}`)
                     .listen('.endtrip', (data) => {
                         const tripId = data.tripId;
-                        const driver = data.driver;         
+                        const driver = data.driver;
                         const message = data.message.replace(/<.*?>/g, ''); // Remove HTML tags
-                        showPopup(message, tripId, driver);
+                        showEnd(message, tripId, driver);
                     });
             } else {
                 console.error('Echo is not initialized!');
             }
         };
 
-        setupPopUp();
+        setupEnd();
     });
 
     function extractTripIdFromMessage(message) {
@@ -116,15 +116,15 @@
         return match ? match[1] : null;
     }
 
-    function showPopup(message, tripId, driver) {
-        document.getElementById('popup-message').textContent = message;
-        document.getElementById('popup-trip-id').value = tripId;
-        document.getElementById('popup-driver-id').value = driver;
-        document.getElementById('popup-modal').classList.remove('hidden');
+    function showEnd(message, tripId, driver) {
+        document.getElementById('end-message').textContent = message;
+        document.getElementById('end-trip-id').value = tripId;
+        document.getElementById('end-driver-id').value = driver;
+        document.getElementById('end-modal').classList.remove('hidden');
     }
 
-    document.getElementById('popup-close').addEventListener('click', function () {
-        document.getElementById('popup-modal').classList.add('hidden');
+    document.getElementById('end-close').addEventListener('click', function () {
+        document.getElementById('end-modal').classList.add('hidden');
     });
 
 
